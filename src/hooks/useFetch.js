5231f327120dbd7ee9cfa436281d239f8e9624cf
marcelo-betassa <mhbeta@gmail.com>
2022-03-06 01:19:49 -0300

    custom hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+
+
+export const useFetch = (url, method = 'GET') => {
+   const [response, setResponse] = useState({
+       data: null,
+       loading: true
+   });
+
+   useEffect(() => {
+        fetch(url,{ method })
+        .then( response => response.json())
+        .then( json => setResponse({
+            data: json,
+            loading: false
+        }));
+   },[url,method]);
+
+   return response;
+}
+
+
+
--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import useCounter from '../../hooks/useCounter'
+import { useFetch } from '../../hooks/useFetch'
 
 const UseRef = (props) => {
+
+    const [count,add1,dec] = useCounter(7);
+    const url = "http://files.cod3r.com.br/curso-react/estados.json";
+
+    const response = useFetch(url);
+
+    function showStates(states) {
+        console.log('states###', states);
+        if (Array.isArray(states)) {
+            return states.map( state =>  <li key={state.nome}>{state.nome} - {state.sigla}</li>);
+        }
+    }
+
     return (
         <div className="UseCustom">
-            <PageTitle
-                title="Seu Hook"
-                subtitle="Vamos aprender como criar o nosso próprio Hook!"
-            />
+            <PageTitle title="Seu Hook" subtitle="Vamos aprender como criar o nosso próprio Hook!" />
+
+            <SectionTitle title="Exercicio #01" />
+            <div className="center">
+                    <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={() => dec()}>-1</button>
+                    <button className="btn" onClick={() => add1()}>+1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercicio #02" />
+            <div className="center">
+                <ul>
+                    {!response.loading ? showStates(response.data) : '' }
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
 
-export default UseRef
+export default UseRef;
